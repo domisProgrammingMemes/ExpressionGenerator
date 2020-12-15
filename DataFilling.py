7441151c3b258a2d1936e2c4cd16f15a412ff016
domisProgrammingMemes <47449002+domisProgrammingMemes@users.noreply.github.com>
2020-12-15 17:17:17 +0100

    Data filling is done on the preprocessed csv. This means missing values are linearly interpolated. There are never 2 consecutive missing values so this can be done to fasten up the preprocessing. This script also removes the old (leaky) preprocessed files for less memory consumption. filled csvs contain "_fill" in their name!

--- DataFilling.py
+++ DataFilling.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
-import os, os.path
+import os
+import os.path
 
 csv_read_path = r"Data\FaceTracker\preprocessed\csv"
 csv_write_path = r"Data\FaceTracker\preprocessed\csv\\"
 
-csv_list = os.listdir(csv_read_path)
-number_of_csv_files = len(csv_list)
-
-step = 1
 
 if __name__ == "__main__":
 
+    csv_list = os.listdir(csv_read_path)
+    number_of_csv_files = len(csv_list)
+
+    step = 1
+
     for csv in csv_list:
         if "_fill" in csv:
-            break
+            pass
         else:
             df = pd.read_csv(csv_read_path + "/" + csv)
             # print head for testing
@@ -37,3 +39,7 @@ if __name__ == "__main__":
             csv = name + "_fill." + type
 
             df.to_csv(path_or_buf=csv_write_path + csv, index=False)
+
+            # delete the file with holes
+            os.remove(r"D:\Hochschule RT\Masterthesis\Coding\ExpressionGenerator\Data\FaceTracker\preprocessed\csv\\" + name + "." + type)
+
