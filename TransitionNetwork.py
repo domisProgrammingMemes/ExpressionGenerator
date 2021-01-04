ea6f4fdfdb326ef080ff763f8c1b703847c17b4f
domisProgrammingMemes <47449002+domisProgrammingMemes@users.noreply.github.com>
2021-01-04 18:48:18 +0100

    training works but I don't know if the loss is calculated properly same goes for testing
    __
    
    cant figure out how to create animations with this set up too

--- TransitionNetwork.py
+++ TransitionNetwork.py
@@ -1,4 +1,5 @@
 import math
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,45 +15,58 @@ from CSVDataLoader import PadSequencer
 
 csv_read_path = r"Data\FaceTracker\preprocessed\csv"
 
-# Path to save and load models
-net_path = "./models/Transition_net.pth"
-
 
 # set up the divice (GPU or CPU) via input prompt
-def set_device():
-    cuda_true = input("Use GPU? (y) or (n)?")
-    if cuda_true == "y":
-        device = "cuda"
-    else:
-        device = "cpu"
-    print("Device:", device)
+cuda_true = input("Use GPU? (y) or (n)?")
+if cuda_true == "y":
+    device = "cuda"
+else:
+    device = "cpu"
+print("Device:", device)
 
 
 # save a networks parameters for future use
-def save_network(net: nn.Module, path):
+def save_network(net: nn.Module, version):
     # save the network?
     save = input("Save net? (y) or (n)?")
     if save == "y":
+        path = "./models/Transition_" + str(version) + "_net.pth"
         torch.save(net.state_dict(), path)
     else:
         pass
 
 
 # load an existing network's parameters and safe them into the just created net
-def load_network(net: nn.Module, net_path):
+def load_network(net: nn.Module):
     # save the network?
     load = input("Load Network? (y) or (n)?")
+
     if load == "y":
-        net.load_state_dict(torch.load(net_path))
+        version = input("Which model should be loaded? (Version number): ")
+        try:
+            int(version)
+        except ValueError:
+            print("That is not a Version number!")
+            version = input("Which model should be loaded? (Version number): ")
+
+        try:
+            path = "./models/Transition_" + str(version) + "_net.pth"
+            net.load_state_dict(torch.load(path))
+
+        except FileNotFoundError:
+            print("There is no such Version yet!")
+            load_network(net)
     else:
         pass
 
 
 # Hyperparameters
-num_epochs = 4
+num_epochs = 1
 train_batch_size = 5
-test_batch_size = 5
-learning_rate = 1e-3
+test_batch_size = 1
+learning_rate = 5e-5
+
+teacher_forcing_ratio = 0.1
 
 # input_size = ?
 # sequence_length = ?
@@ -61,6 +75,7 @@ learning_rate = 1e-3
 transforms = transforms.ToTensor()
 
 torch.manual_seed(0)
+
 
 if __name__ == "__main__":
 
@@ -71,7 +86,7 @@ if __name__ == "__main__":
     # print("test_before_loader.type():", test_before_loader.type())
 
     trainloader = DataLoader(dataset=trainset, batch_size=train_batch_size, collate_fn=PadSequencer(), shuffle=True, num_workers=0, drop_last=False)
-    testloader = DataLoader(dataset=testset, batch_size=train_batch_size, collate_fn=PadSequencer(), shuffle=True, num_workers=0, drop_last=False)
+    testloader = DataLoader(dataset=testset, batch_size=test_batch_size, collate_fn=PadSequencer(), shuffle=True, num_workers=0, drop_last=False)
 
 
     """
@@ -127,7 +142,7 @@ if __name__ == "__main__":
         def __init__(self, input_size: int, hidden_size: int):
             super(Encoder, self).__init__()
             # input to hidden 1
-            self.fc1 = nn.Linear(input_size, hidden_size)
+            self.fc1 = nn.Linear(input_size, hidden_size, bias=False)
             self.fc2 = nn.Linear(hidden_size, hidden_size)
 
         def forward(self, x):
@@ -186,11 +201,12 @@ if __name__ == "__main__":
             return lstm_out, h_t, c_t
 
 
-        def init_hidden(self):
-            h0 = torch.zeros(self.num_layers, train_batch_size, self.hidden_size)
-            c0 = torch.zeros(self.num_layers, train_batch_size, self.hidden_size)
+        def init_hidden(self, batch_size):
+            h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+            c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+            h0 = h0.to(device=device)
+            c0 = c0.to(device=device)
             return h0, c0
-
 
 
     class TransitionNetwork(nn.Module):
@@ -251,74 +267,211 @@ if __name__ == "__main__":
 
             return next_frame, hidden_state, cell_state
 
-
             # self.count += 1
             # return self.count
 
+    # def transition_network_loss(predicted_frame, real_frame):
+    #     loss = F.mse_loss(predicted_frame.view(-1, 15), real_frame.view(-1, 15))
+    #     return loss
 
-
-
-    # Testing stuff:
 
     # Transition Network
-    MyModel = TransitionNetwork(15)
+    TransitionAE = TransitionNetwork(15)
+    load_network(TransitionAE)
 
-    for index, data in enumerate(trainloader):
-        batch_features, lengths, names = data
-
-        print(f"before model - batch_features.size()", batch_features.size())
-        print(f"before model - batch_features[0].size()", batch_features[0].size())
-        print(f"before model - lengths:", lengths)
-        print(f"before model - names: {names}")
-        # for batch_idx in batch_features:
-        #     print("batch_idx", batch_idx.size())
-        # batch is: [batch_size, sequence_length, feature_size]
-        # calculate current frame, offset and target
-
-        # TODO: Tell each batch how long the sequence is so it can ignore everything after the last frame somehow?
-
-        print(f"++++++++++++++++++++++++++++++ index (consists of a batch of batch_size): {index} +++++++++++++++++++++++++++++++++")
-
-        # targets for all batches (hyperparams)
-        target = torch.empty(train_batch_size, batch_features.size(2))
-        for i in range(train_batch_size):
-            target[i] = batch_features[i][lengths[i]-1][:]
-
-        # init hidden per batch
-        hidden, cell = MyModel.lstm.init_hidden()
-
-        print(f"before model - target.size(): {target.size()}")
-        print(f"before model - target: {target}")
-        print()
-        print("PER FRAME CALCULATIONS")
-        print()
-
-        for frame in range(lengths[0]):
-            # try:
-            #     print(f"====================> frame: {count}")
-            # except:
-            #     print(f"====================> frame: 0")
-            # count = MyModel.forward(batch_features[:, frame, :], target)
-            # print(f"----------------------- new frame ----------------------")
-            # print(batch_features[:, frame, :])
-            next_frame, hidden, cell = MyModel.forward(batch_features[:, frame, :], target, hidden, cell)
-
-            # target
-            if frame < (lengths[0] - 1):
-                real_next_frame = batch_features[:, frame+1, :]
-            else:
-                real_next_frame = target
-            print(real_next_frame)
+    TransitionAE.to(device=device)
 
 
+    optimizer = optim.Adam(TransitionAE.parameters(), lr=learning_rate)
+    criterion = nn.MSELoss()
 
-            # print(hidden)
-            # print(cell)
-            # print(next_frame.size())
+    def train(nEpochs: int):
+        TransitionAE.train()
+        print("Training...")
 
 
-        # just on batch for now!!!
-        break
+        for epoch in range(nEpochs):
+            running_loss = 0.0
+
+            for index, data in enumerate(trainloader):
+                batch_features, lengths, names = data
+                batch_features = batch_features.to(device=device)
+                # lengths = lengths.to(device=device)
+
+                # print(f"before model - batch_features.size()", batch_features.size())
+                # print(f"before model - batch_features[0].size()", batch_features[0].size())
+                # print(f"before model - lengths:", lengths)
+                # print(f"before model - names: {names}")
+
+                # for batch_idx in batch_features:
+                #     print("batch_idx", batch_idx.size())
+                # batch is: [batch_size, sequence_length, feature_size]
+                # calculate current frame, offset and target
+
+                # print(
+                #     f"++++++++++++++++++++++++++++++ index (consists of a batch of batch_size): {index} +++++++++++++++++++++++++++++++++")
+
+
+                # targets for all batches (hyperparams)
+                target = torch.empty(train_batch_size, batch_features.size(2))
+                for i in range(train_batch_size):
+                    target[i] = batch_features[i][lengths[i] - 1][:]
+
+                # target to device
+                target = target.to(device=device)
+
+                # init hidden per batch
+                hidden, cell = TransitionAE.lstm.init_hidden(train_batch_size)
+
+
+                # print(f"before model - target.size(): {target.size()}")
+                # print(f"before model - target: {target}")
+                # print()
+                # print("PER FRAME CALCULATIONS")
+                # print()
+
+                for frame in range(lengths[0]):
+                    # try:
+                    #     print(f"====================> frame: {count}")
+                    # except:
+                    #     print(f"====================> frame: 0")
+                    # count = TransitionAE.forward(batch_features[:, frame, :], target)
+                    # print(f"----------------------- new frame ----------------------")
+                    # print(batch_features[:, frame, :])
+
+
+                    # teacher forcing with prob = 0.2
+                    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+                    if frame < (lengths[0] - 1):
+                        real_next_frame = batch_features[:, frame + 1, :]
+                    else:
+                        real_next_frame = target
+
+                    # when teacher forcing true: feed the real next_frame to the network
+                    if use_teacher_forcing:
+                        predicted_next_frame = real_next_frame
+
+                    predicted_next_frame, hidden, cell = TransitionAE(batch_features[:, frame, :], target, hidden,
+                                                                      cell)
+
+                    hidden.detach_()
+                    cell.detach_()
+
+                    # print(f"after model - predicted_next_frame: {predicted_next_frame.size()}")
+                    # print(f"after model - real_next_frame: {real_next_frame.size()}")
+
+
+                    loss = criterion(predicted_next_frame, real_next_frame)
+                    # loss = transition_network_loss(predicted_next_frame, real_next_frame)
+                    # print(f"after model - loss: {loss}")
+
+                    # backprop
+                    optimizer.zero_grad()
+                    loss.backward()
+
+                    # one step of the optimizer
+                    optimizer.step()
+
+                    running_loss += loss.item()
+
+                    # print(hidden)
+                    # print(cell)
+                    # print(predicted_next_frame.size())
+
+                    # end frame
+
+                # end batch -> all frames
+                if index % 1 == 1 - 1:
+                    print(f"epoch: {epoch + 1}, mini-batch: {index + 1} - loss: {running_loss}")
+                    running_loss = 0.0
+
+            # end epoch
+            # print(f"epoch: {epoch + 1}")
+
+
+        print("Training finished!")
+        save_network(TransitionAE, 0)
+
+    def test(loader: DataLoader, model):
+        model.eval()
+
+        with torch.no_grad():
+            running_loss = 0.0
+            all_loss = 0.0
+
+            for index, data in enumerate(loader):
+                batch_features, lengths, names = data
+                batch_features = batch_features.to(device=device)
+
+                # targets for all batches (hyperparams)
+                target = torch.empty(test_batch_size, batch_features.size(2))
+                for i in range(test_batch_size):
+                    target[i] = batch_features[i][lengths[i] - 1][:]
+
+                # target to device
+                target = target.to(device=device)
+
+                # init hidden per batch
+                hidden, cell = TransitionAE.lstm.init_hidden(test_batch_size)
+
+                for frame in range(lengths[0]):
+
+                    if frame < (lengths[0] - 1):
+                        real_next_frame = batch_features[:, frame + 1, :]
+                    else:
+                        real_next_frame = target
+
+                    # print(batch_features[:, frame, :].size())
+                    # exit()
+
+                    predicted_next_frame, hidden, cell = TransitionAE(batch_features[:, frame, :], target, hidden,
+                                                                          cell)
+
+
+                    loss = criterion(predicted_next_frame, real_next_frame)
+                    running_loss += loss.item()
+                    # end frame
+                
+                # end batch
+                print(f"Running Loss: {running_loss}")
+                all_loss += running_loss
+                running_loss = 0.0
+
+            print(f"all losses: {all_loss}")
+
+
+    # def generate_sequence(model, start_frame, target_frame, duration):
+    #     model.eval()
+    #     transition = []
+    #
+    #     start_frame = torch.unsqueeze(start_frame, 0).to(device=device)
+    #     target_frame = torch.unsqueeze(target_frame, 0).to(device=device)
+    #
+    #     hidden = None
+    #     cell = None
+    #
+    #     with torch.no_grad():
+    #
+    #         for i in range(duration-1):
+    #             predicted_next_frame, hidden, cell = TransitionAE(start_frame, target_frame, hidden, cell)
+    #             transition.append(predicted_next_frame)
+    #         return transition
+
+    # train(num_epochs)
+    # test(testloader, TransitionAE)
+
+    start = torch.tensor([1.1735114637781786e-08,7.241975343010249e-08,0.018730973801541314,0.09275460610676707,1.1999998418906612,1.176913967268185,0.1643039178064644,0.18280748342566253,0.17407648701965087,0.2411355052111981,0.229750059760292,3.626090594941871e-08,1.2085446863230588e-09,8.463887096639834e-11,2.5167801468923616e-10])
+    end = torch.tensor([1.1999996482175648,1.199999459762757,1.199999349722415,1.1999967071002502,3.730670327388917e-07,2.192692481468664e-05,0.8932812902538821,0.9212007896416295,3.333123421165009e-07,1.7824243257347232e-07,3.263498064039033e-07,3.45552188066782e-07,4.02158899625008e-06,0.05982334779796216,1.1986696864049995])
+    #print(start.size())
+    #print(end.size())
+    t = 10
+
+    # transition = generate_sequence(TransitionAE, start, end, t)
+    # print(transition)
+
+
+
 
 
 
