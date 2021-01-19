8a9d2f7c2653ef1e073a75673b7db24e88f15dff
domisProgrammingMemes <47449002+domisProgrammingMemes@users.noreply.github.com>
2021-01-19 18:15:38 +0100

    prep for triple_dataset change date for writers look up clipping as gradients explode around 50ish epochs

--- TransitionNetwork.py
+++ TransitionNetwork.py
@@ -70,7 +70,7 @@ def load_network(net: nn.Module):
             version = input("Which model should be loaded? (Version number): ")
 
         try:
-            path = "./models/normal_dataset/ExGen_" + str(version) + "_net.pth"
+            path = "./models/triple_dataset/ExGen_" + str(version) + "_net.pth"
             net.load_state_dict(torch.load(path))
 
         except FileNotFoundError:
@@ -96,7 +96,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 if __name__ == "__main__":
 
     dataset = AUDataset(csv_read_path)
-    trainset, valset, testset = torch.utils.data.random_split(dataset, [60, 13, 12])
+    trainset, valset, testset = torch.utils.data.random_split(dataset, [205, 25, 25])
 
     # trainloader = DataLoader(dataset=trainset, batch_size=train_batch_size, collate_fn=PadSequencer(), shuffle=True, num_workers=0, drop_last=True)
     # testloader = DataLoader(dataset=testset, batch_size=test_batch_size, collate_fn=PadSequencer(), shuffle=True, num_workers=0, drop_last=True)
@@ -128,8 +128,8 @@ if __name__ == "__main__":
             self.rnn = nn.LSTM(n_output_encoder, n_hidden, num_layers=n_layers, batch_first=True)
             # decoder -> 3 fc-layers
             self.decoder1 = nn.Linear(n_hidden, n_output_encoder)
-            self.decoder2 = nn.Linear(n_output_encoder, n_output_encoder / 4)
-            self.decoder3 = nn.Linear(n_output_encoder / 4, n_features)
+            self.decoder2 = nn.Linear(n_output_encoder, int(n_output_encoder / 4))
+            self.decoder3 = nn.Linear(int(n_output_encoder / 4), n_features)
             # batch norm
             # self.batch_norm_encoder = nn.BatchNorm1d(n_output_encoder)
             # self.batch_norm_lstm = nn.BatchNorm1d(n_hidden)
@@ -180,17 +180,17 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
 
-    # best current test error (MSE or L1?):
-    best_error = 100                    # none so far
-    last_epoch = 0                      # nothing done so far
+    # best current test error (MSE):
+    best_error = 2.8526
+    last_epoch = 0
 
-    # model_safe = 15_256_512 | features, encoded_size, hidden_size
+    # model_safe = 15_256_512 | features, encoded_size, hidden_size; 0_ is best model (MSE) from train, 1_ is last model from train
 
     # training loop
     def train_model(train: DataLoader, val: DataLoader, n_Epochs: int, best_test_error: float):
         writer = SummaryWriter()
         loss_history = []
-        best_epoch = 0                  # nothing done so far
+        best_epoch = 0
         print("Start training...")
 
         for epoch in range(1 + last_epoch, n_Epochs + 1 + last_epoch):
@@ -261,6 +261,10 @@ if __name__ == "__main__":
 
                 loss = mse_loss(created_sequence, batch_data)
                 loss.backward()
+
+                # grad clipping
+                nn.utils.clip_grad_value_(model.parameters(), clip_value=0.5)
+
                 optimizer.step()
 
                 train_loss = train_loss + loss.item()
@@ -316,25 +320,23 @@ if __name__ == "__main__":
             # if val loss last worse than new val loss safe model - KOMMT NOCH
             # val loss with L1 Loss (am besten auch MSE einfach zum vgl!)
             if val_loss_mse < best_test_error:
-                torch.save(model.state_dict(), "./models/normal_dataset/ExGen_0_15_256_512_net.pth")
+                torch.save(model.state_dict(), "./models/triple_dataset/ExGen_0_15_256_512_net.pth")
                 best_test_error = val_loss_mse
                 best_epoch = epoch
-                print("new Model was saved!")
+                print("New Model had been saved!")
 
             # adjust lr (by hand now for better results?
             # scheduler.step()
 
         # append to txt .. better save than sorry!
-        with open(r'training_history\history_normal_1901_15_256_512.txt', 'a') as f:
+        with open(r'training_history\history_triple_1901_15_512.txt', 'a') as f:
             print(loss_history, file=f)
 
         writer.close()
-        print("best test error now (for copy-paste):", best_test_error)
-        print("epoch of best test error:", best_epoch)
+        print("Best test error (for copy-paste):", best_test_error)
+        print("Epoch (best test error):", best_epoch)
         print("Finished training!")
-        torch.save(model.state_dict(), "./models/normal_dataset/ExGen_1_15_256_512_net.pth")
-
-    train_model(train_loader, val_loader, num_epochs, best_error)
+        torch.save(model.state_dict(), "./models/triple_dataset/ExGen_1_15_256_512_net.pth")
 
     def test_model(test: DataLoader):
         model.eval()
@@ -376,13 +378,32 @@ if __name__ == "__main__":
                 test_loss_l1 = test_loss_l1 + loss_l1.item()
 
         print(f"Test_losses: MSE = {test_loss_mse:.4f} | L1 = {test_loss_l1:.4f}")
+        with open(r'test_history\test_triple_1901_15_256_512.txt', 'a') as f:
+            print(f"MSE:{test_loss_mse}, L1:{test_loss_l1}", file=f)
 
+
+    train_model(train_loader, val_loader, num_epochs, best_error)
+    test_model(test_loader)
 
     ####### GENERATION #######
 
-    # frown to happy (not in data!)
-    start = torch.Tensor([0.06131799283585899,0.08172666935186508,0.03395699517793453,0.04818290386941117,1.1412230401159085,1.1035029605516014,0.113120523744227,1.0597114447811183e-07,0.30072830873532025,3.517824015742137e-05,8.749662808520226e-09,0.0013996257573782305,0.6260813195906696,0.09551505350147324,6.689342889722282e-10])
-    end = torch.Tensor([1.1999996314449428,1.1999995870728282,1.0278275208869838,1.0194900942312504,0.2341106233151145,0.0653982846386197,0.8537125658024893,0.8081682829103988,1.4364089873377762e-08,-2.1860498663560296e-08,9.06581770646935e-07,0.006658209848300654,-4.7158831256122355e-09,6.94503947633974e-07,0.9977853517181079])
+    # frown to suprised (not in data!)
+    # start = torch.Tensor([0.06131799283585899,0.08172666935186508,0.03395699517793453,0.04818290386941117,1.1412230401159085,1.1035029605516014,0.113120523744227,1.0597114447811183e-07,0.30072830873532025,3.517824015742137e-05,8.749662808520226e-09,0.0013996257573782305,0.6260813195906696,0.09551505350147324,6.689342889722282e-10])
+    # end = torch.Tensor([1.1999996314449428,1.1999995870728282,1.0278275208869838,1.0194900942312504,0.2341106233151145,0.0653982846386197,0.8537125658024893,0.8081682829103988,1.4364089873377762e-08,-2.1860498663560296e-08,9.06581770646935e-07,0.006658209848300654,-4.7158831256122355e-09,6.94503947633974e-07,0.9977853517181079])
+
+    # disgust1 to happy5 (more or less in data)
+    # start = torch.Tensor([6.716317271550969e-08, 0.02935825960378735, 0.021760095040568358, 0.08411739822017017, 1.1999998885514678, 1.1846442213750967, 0.2946981952170156, 0.14367314787076094, 0.0014814104832844687, 0.30272492210729074, 0.3789290959138957, 1.1742008603708393e-08, 4.753175528420333e-09, 4.965092547141178e-09, 1.8304739182918754e-09])
+    # end = torch.Tensor([0.06125034864614825,0.02625214569476484,4.114325553268089e-09,0.02274558697252165,0.17967660446464898,0.13432276325223844,0.15313606416869646,0.12118201135523902,3.097393968560188e-08,9.003948174507734e-10,0.1875999910930843,-3.096835818564804e-11,-1.6041286239695211e-10,2.658632689948891e-10,0.041449114407351585])
+
+    # disgust_suprised5 end
+    # end = torch.Tensor([1.2000000063515006,1.2000000037242264,1.20000000988556,1.20000000517873,-6.258249112550545e-09,-5.344243176490128e-09,0.8856496208847644,0.7621254478783975,2.209424007560319e-09,-1.967204045131492e-08,9.93416993606448e-07,0.31120050895524043,3.8454029649169626e-07,0.18082177539922012,0.9725068273045891])
+
+    # sup_neutral2 end
+    # end = torch.Tensor([0.07738269721417339,0.08807293900396353,0.011010792242784829,0.06143003939298607,0.029883286893899432,1.3437291391474005e-10,6.630145104358612e-10,1.0842664182092701e-10,4.41634207694271e-11,1.8048909959586743e-11,1.7592160096523457e-10,1.8230500751428678e-11,0.19040087231317004,7.341144061507404e-11,1.1859499592224213e-09])
+
+    # start happy_neutral3
+    # start = torch.Tensor([1.7312022381517743e-07,9.942494218012084e-07,5.5279378574193394e-08,7.275862012056518e-08,0.1614868551455088,0.10531355676472627,1.199833379749608,1.1999999171354383,6.253415944733785e-08,0.25089657727780995,0.5664661956248268,1.0567180085991217,5.8532759285127815e-09,4.522632508438027e-09,1.2681872116221288e-07])
+
     # print(start.size())
     # exit()
 
@@ -428,7 +449,7 @@ if __name__ == "__main__":
             del df
 
             # generate new name for the generated animation
-            new_name = "Gen_" + name
+            new_name = "TEST_Triple" + name
 
             # transform predictions to csv
             sequence_np = sequence.numpy()
@@ -439,7 +460,7 @@ if __name__ == "__main__":
             del sequence_np
             del sequence_df
 
-    # generate_expression(start, end, 180, "frown_to_happy")
+    # generate_expression(start, end, 220, "Test_triple_DURATION_WHICHMODEL_FROM_to_TO")
 
 
     # custom safe method which can be used to store individual models (name as input during method)
