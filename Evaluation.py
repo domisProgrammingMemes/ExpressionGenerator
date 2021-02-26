a91fa3033baea1087b3e95c71ef53f74d4e3de96
domisProgrammingMemes <47449002+domisProgrammingMemes@users.noreply.github.com>
2021-02-26 14:34:10 +0100

    slight changes to plotting no legend, one more function for easier access

--- Evaluation.py
+++ Evaluation.py
@@ -20,8 +20,6 @@ y_indexes_reduced = np.arange(len(AUs_reduced))
 
 def compare_two_sequences(GT: str, ExGen: str):
 
-    fig = plt.figure()
-
     data = {}
     data2 = {}
     GT_data = pd.read_csv(GT)
@@ -45,16 +43,18 @@ def compare_two_sequences(GT: str, ExGen: str):
     array2 = [data2[i].values for i in range(0, 16)]
 
 
-    gs = GridSpec(1, 2, figure=fig)
+    # gs = GridSpec(1, 2, figure=fig)
 
     # ===========================
     # first subplot: Ground Truth
     # ===========================
-    ax = fig.add_subplot(gs[0, 0], projection="3d")
+    # ax = fig.add_subplot(gs[0, 0], projection="3d")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
     for i in range(1, 16):
         ax.plot(array[0], array[i], i - 1, zdir="y", color=colors[i - 1])
 
-    ax.set_title("GT: disgust to happy", y=.84, pad=0, fontsize=15)
+    # ax.set_title("Im Datensatz", y=.84, pad=0, fontsize=15)
     ax.set_xlabel("Frame", fontsize=14)
     ax.set_xlim(duration, 0)
     ax.set_ylim(0, 15)
@@ -67,15 +67,22 @@ def compare_two_sequences(GT: str, ExGen: str):
     ax.set_zlim(0, 1.25)
     ax.view_init(elev=4, azim=340)
     ax.tick_params(axis="both", labelsize=12)
+
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
 
     # ====================================
     # second subplot: Expression Generator
     # ====================================
-    ax = fig.add_subplot(gs[0, 1], projection="3d")
+    # ax = fig.add_subplot(gs[0, 1], projection="3d")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
     for i in range(1, 16):
         ax.plot(array2[0], array2[i], i - 1, zdir="y", color=colors[i - 1])
 
-    ax.set_title("ExGen: disgust to happy", y=.84, pad=0, fontsize=15)
+    # ax.set_title("Variation", y=.84, pad=0, fontsize=15)
     ax.set_xlabel("Frame", fontsize=14)
     ax.set_xlim(duration, 0)
     ax.set_ylim(0, 15)
@@ -89,12 +96,13 @@ def compare_two_sequences(GT: str, ExGen: str):
     ax.tick_params(axis="both", labelsize=12)
 
 
-    fig.subplots_adjust(top=0.985,
-                        bottom=0.015,
-                        left=0.008,
-                        right=0.991,
-                        hspace=0.2,
-                        wspace=0.018)
+    # fig.subplots_adjust(top=0.985,
+    #                     bottom=0.015,
+    #                     left=0.008,
+    #                     right=0.991,
+    #                     hspace=0.2,
+    #                     wspace=0.018)
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
     mng = plt.get_current_fig_manager()
     mng.window.showMaximized()
 
@@ -128,10 +136,7 @@ def compare_two_sequences(GT: str, ExGen: str):
     # ax.add_collection3d(poly, zs=zs, zdir='y')
 
 
-
-
-
-    ax.set_title("Comparison", y=.86, pad=0, fontsize=15)
+    # ax.set_title("Vergleich", y=.86, pad=0, fontsize=15)
     ax.set_xlabel("Frame", fontsize=14)
     ax.set_xlim(duration, 0)
     ax.set_ylim(0, 15)
@@ -148,6 +153,177 @@ def compare_two_sequences(GT: str, ExGen: str):
     # ==========
     # show plots
     # ==========
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+
+
+
+def compare_two_sequences_var(GT: str, ExGen: str, ExGen2: str):
+
+    data = {}
+    data2 = {}
+    data3 = {}
+    GT_data = pd.read_csv(GT)
+    ExGen_data = pd.read_csv(ExGen)
+    ExGen_data2 = pd.read_csv(ExGen2)
+    duration = GT_data.iloc[-1, 0]
+
+    # ====
+    # Data
+    # ====
+    data[0] = GT_data.iloc[:duration, 0]
+    for i in range(1, 16):
+        data[i] = GT_data.iloc[:duration, i]
+
+    array = [data[i].values for i in range(0, 16)]
+
+    data2[0] = data[0]
+    # data2[0] = GT_data.iloc[:duration, 0]
+    for i in range(1, 16):
+        data2[i] = ExGen_data.iloc[:duration, i]
+
+    array2 = [data2[i].values for i in range(0, 16)]
+
+    data3[0] = data[0]
+    # data2[0] = GT_data.iloc[:duration, 0]
+    for i in range(1, 16):
+        data3[i] = ExGen_data2.iloc[:duration, i]
+
+    array3 = [data3[i].values for i in range(0, 16)]
+
+    # gs = GridSpec(1, 2, figure=fig)
+
+    # ===========================
+    # first subplot: Ground Truth
+    # ===========================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    for i in range(1, 16):
+        ax.plot(array[0], array[i], i - 1, zdir="y", color=colors[i - 1])
+
+    # ax.set_title("Im Datensatz", y=.84, pad=0, fontsize=15)
+    ax.set_xlabel("Frame", fontsize=14)
+    ax.set_xlim(duration, 0)
+    ax.set_ylim(0, 15)
+    ax.set_yticks(ticks=y_indexes)
+    ax.set_yticklabels(AUs, rotation=270)
+    for ytick, color in zip(ax.get_yticklabels(), colors):
+        ytick.set_color(color)
+
+    ax.set_zlabel("AU-Intensity", fontsize=14)
+    ax.set_zlim(0, 1.25)
+    ax.view_init(elev=4, azim=340)
+    ax.tick_params(axis="both", labelsize=12)
+
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+    # ====================================
+    # second subplot: Expression Generator
+    # ====================================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    for i in range(1, 16):
+        ax.plot(array2[0], array2[i], i - 1, zdir="y", color=colors[i - 1])
+
+    ax.set_xlabel("Frame", fontsize=14)
+    ax.set_xlim(duration, 0)
+    ax.set_ylim(0, 15)
+    ax.set_yticks(ticks=y_indexes)
+    ax.set_yticklabels(AUs, rotation=270)
+    for ytick, color in zip(ax.get_yticklabels(), colors):
+        ytick.set_color(color)
+    ax.set_zlabel("AU-Intensity", fontsize=14)
+    ax.set_zlim(0, 1.25)
+    ax.view_init(elev=4, azim=340)
+    ax.tick_params(axis="both", labelsize=12)
+
+
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+    # =======================================
+    # third subplot: Expression Generator var
+    # =======================================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    for i in range(1, 16):
+        ax.plot(array3[0], array3[i], i - 1, zdir="y", color=colors[i - 1])
+
+    ax.set_xlabel("Frame", fontsize=14)
+    ax.set_xlim(duration, 0)
+    ax.set_ylim(0, 15)
+    ax.set_yticks(ticks=y_indexes)
+    ax.set_yticklabels(AUs, rotation=270)
+    for ytick, color in zip(ax.get_yticklabels(), colors):
+        ytick.set_color(color)
+    ax.set_zlabel("AU-Intensity", fontsize=14)
+    ax.set_zlim(0, 1.25)
+    ax.view_init(elev=4, azim=340)
+    ax.tick_params(axis="both", labelsize=12)
+
+
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+
+    # ====================================
+    # fourth plot: 2 in 1 org
+    # ====================================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    width = 0.0
+    for i in range(1, 16):
+        ax.plot(array[0], array[i], i - 1 + width, zdir="y", color=colors[i - 1], linestyle="--")
+        ax.plot(array2[0], array2[i], i - 1, zdir="y", color=colors[i - 1])
+        ax.add_collection3d(plt.fill_between(array[0], array[i], array2[i], color=colors[i-1], alpha=0.1), zs=i-1, zdir='y')
+
+    ax.set_xlabel("Frame", fontsize=14)
+    ax.set_xlim(duration, 0)
+    ax.set_ylim(0, 15)
+    ax.set_yticks(ticks=y_indexes)
+    ax.set_yticklabels(AUs, rotation=270)
+    for ytick, color in zip(ax.get_yticklabels(), colors):
+        ytick.set_color(color)
+    ax.set_zlabel("AU-Intensity", fontsize=14)
+    ax.set_zlim(0, 1.25)
+    ax.view_init(elev=6, azim=350)
+    ax.tick_params(axis="both", labelsize=12)
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+    # ====================================
+    # fifth plot: 2 in 1 var
+    # ====================================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    width = 0.0
+    for i in range(1, 16):
+        ax.plot(array[0], array[i], i - 1 + width, zdir="y", color=colors[i - 1], linestyle="--")
+        ax.plot(array3[0], array3[i], i - 1, zdir="y", color=colors[i - 1])
+        ax.add_collection3d(plt.fill_between(array[0], array[i], array3[i], color=colors[i - 1], alpha=0.1), zs=i - 1,
+                            zdir='y')
+
+    ax.set_xlabel("Frame", fontsize=14)
+    ax.set_xlim(duration, 0)
+    ax.set_ylim(0, 15)
+    ax.set_yticks(ticks=y_indexes)
+    ax.set_yticklabels(AUs, rotation=270)
+    for ytick, color in zip(ax.get_yticklabels(), colors):
+        ytick.set_color(color)
+    ax.set_zlabel("AU-Intensity", fontsize=14)
+    ax.set_zlim(0, 1.25)
+    ax.view_init(elev=6, azim=350)
+    ax.tick_params(axis="both", labelsize=12)
+    fig.subplots_adjust(top=1, bottom=0.0, left=0.0, right=1.0, hspace=0, wspace=0)
+
     mng = plt.get_current_fig_manager()
     mng.window.showMaximized()
 
@@ -227,8 +403,6 @@ def plot_loss(path: str):
 
 def plot_differences(GT: str, ExGen: str, ExGen2: str):
 
-    fig = plt.figure()
-
     data = {}
     data2 = {}
     GT_data = pd.read_csv(GT)
@@ -243,7 +417,8 @@ def plot_differences(GT: str, ExGen: str, ExGen2: str):
         data[i] = GT_data.iloc[:duration, i] - ExGen_data.iloc[:duration, i]
     array = [data[i].values for i in range(0, 16)]
 
-    ax = fig.add_subplot(1, 2, 1, projection="3d")
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
     for i in range(1, 16):
         ax.plot(array[0], array[i], i - 1, zdir="y", color=colors[i - 1])
     # Stuff
@@ -260,18 +435,29 @@ def plot_differences(GT: str, ExGen: str, ExGen2: str):
     ax.set_zlim(-1.25, 1.25)
     ax.view_init(elev=3, azim=355)
     ax.tick_params(axis="both", labelsize=13)
-    ax.set_title("Difference GT - ExGen: Animation in DS", y=.83, pad=0, fontsize=15)
+    # ax.set_title("Difference GT - ExGen: Animation in DS", y=.83, pad=0, fontsize=15)
 
     # fig.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0.2, wspace=0.2)
+    fig.subplots_adjust(top=1.,
+                        bottom=0.0,
+                        left=0.00,
+                        right=1.,
+                        hspace=0.0,
+                        wspace=0.0)
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
 
     # ###########
     # Second Plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+
     data2[0] = GT_data.iloc[:duration, 0]
     for i in range(1, 16):
         data2[i] = GT_data.iloc[:duration, i] - ExGen_data2.iloc[:duration, i]
     array2 = [data2[i].values for i in range(0, 16)]
 
-    ax = fig.add_subplot(1, 2, 2, projection="3d")
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
     for i in range(1, 16):
         ax.plot(array2[0], array2[i], i - 1, zdir="y", color=colors[i - 1])
     # Stuff
@@ -288,13 +474,13 @@ def plot_differences(GT: str, ExGen: str, ExGen2: str):
     ax.set_zlim(-1.25, 1.25)
     ax.view_init(elev=3, azim=355)
     ax.tick_params(axis="both", labelsize=13)
-    ax.set_title("Difference GT - ExGen: Animation not in DS", y=.83, pad=0, fontsize=15)
+    # ax.set_title("Difference GT - ExGen: Animation not in DS", y=.83, pad=0, fontsize=15)
 
-    fig.subplots_adjust(top=0.985,
-                        bottom=0.015,
-                        left=0.008,
-                        right=0.992,
-                        hspace=0.2,
+    fig.subplots_adjust(top=1.,
+                        bottom=0.0,
+                        left=0.00,
+                        right=1.,
+                        hspace=0.0,
                         wspace=0.0)
     mng = plt.get_current_fig_manager()
     mng.window.showMaximized()
@@ -327,17 +513,18 @@ def plot_differences_box(GT: str, ExGen: str, ExGen2: str):
         plt.setp(bp['boxes'], color=color)
         plt.setp(bp['whiskers'], color=color)
         plt.setp(bp['caps'], color=color)
-        plt.setp(bp['medians'], color=median, linestyle="--", linewidth=2.0)
+        plt.setp(bp['medians'], color=median, linestyle="dotted", linewidth=2.0)
 
 
-    bpl = plt.boxplot(array[1:], positions=np.array(range(len(array[1:]))) * 2.0 - 0.39, sym='', widths=0.72)
+    bpl = plt.boxplot(array[1:], positions=np.array(range(len(array[1:]))) * 2.0 - 0.39, sym='o', widths=0.72)
     set_box_color(bpl, '#5e84ff', "#000000")
-    bpr = plt.boxplot(array2[1:], positions=np.array(range(len(array2[1:]))) * 2.0 + 0.39, sym='', widths=0.72)
+    bpr = plt.boxplot(array2[1:], positions=np.array(range(len(array2[1:]))) * 2.0 + 0.39, sym='+', widths=0.72)
     set_box_color(bpr, '#ff4f4f', "#000000")
 
     # draw temporary red and blue lines and use them to create a legend
     plt.plot([], c='#2747b0', label='Animation im Datensatz')
     plt.plot([], c='#a62d2d', label='Animation nicht im Datensatz')
+    plt.plot([], linestyle="dotted", c='#000000', label='Median')
     plt.legend(fontsize=15)
 
 
@@ -407,10 +594,11 @@ i_frownneutral_v = "./Data/Evaluation/i_testing/ExGen_i_testing_frown2neutral_va
 # plot_loss(trainingshistory)
 
 # ### Sequences ###
-# compare_two_sequences(GT_disgusthappy5, i_disgusthappy5)
 # eval_one_sequence(i_disgusthappy5)
+# compare_two_sequences(GT_frownneutral5, i_frownneutral_v)
+# compare_two_sequences_var(GT_frownneutral5, i_frownneutral5, i_frownneutral_v)
 
 # ### Plot of difference ###
-# plot_differences(GT_disgusthappy5, i_disgusthappy5, i_disgusthappy_v)
-# plot_differences_box(GT_disgusthappy5, i_disgusthappy5, i_disgusthappy_v)
+# plot_differences(GT_frownneutral5, i_frownneutral5, i_frownneutral_v)
+plot_differences_box(GT_frownneutral5, i_frownneutral5, i_frownneutral_v)
 plt.show()
